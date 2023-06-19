4cd11b685d8689e6773a25b0a3303eabd9f1fe7c
Seeqgold <seeqrothgold@gmail.com>
2023-06-19 16:00:34 +0100

    javascript function was added to the review card

--- SCRIPTS/home.js
+++ SCRIPTS/home.js
@@ -1,3 +1,51 @@
+/*const arrows = document.querySelectorAll("[data-carousel-button]")
+
+arrows.forEach(n => n.
+    addEventListener("click", () => {
+        const offset = 
+    }))
+    const carouselSlide= document.querySelector(".review");
+    const carouselContainer= document.querySelectorAll(".review-div");
+
+    const prevBtn = document.querySelector("previous-tag-link");
+    const nextBtn = document.querySelector("next-tag-link");
+
+let counter=1;
+const size = carouselContainer[0].clientWidth;
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+nextBtn.addEventListener('click', ()=>{
+    if(counter >= carouselContainer.length -1) return;
+    carouselSlide.style.transition ="transition 0.4s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    
+})
+
+
+prevBtn.addEventListener('click', ()=>{
+    if(counter <= 0) return;
+    carouselSlide.style.transition ="transition 0.4s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    
+})
+carouselSlide.addEventListener('transitioned', ()=>{
+   
+    if(carouselContainer[counter].id === 'lastClone'){
+        carouselSlide.style.transition ="none";
+        counter= carouselContainer.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+})
+carouselSlide.addEventListener('transitioned', ()=>{
+    if(carouselContainer[counter].id === 'firstClone'){
+        carouselSlide.style.transition ="none";
+        counter= carouselContainer.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+})
+*/
 
 const hamburger= document.querySelector(".hamburger");
 const navItem= document.querySelector(".nav-menu");
@@ -13,5 +61,6 @@ document.querySelectorAll(".nav-link").forEach(n => n.
         navItem.classList.remove("active");
 
 }))
+
 
 
