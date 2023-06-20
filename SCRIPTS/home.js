60821f96cfcee5c03123d53aec2c9f9a0b1215cc
Seeqgold <seeqrothgold@gmail.com>
2023-06-20 11:23:51 +0100

    javascript functions were added to the review card section

--- SCRIPTS/home.js
+++ SCRIPTS/home.js
@@ -47,6 +47,23 @@ carouselSlide.addEventListener('transitioned', ()=>{
 })
 */
 
+const slider = document.querySelector(".carousel-slider");
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector('.right');
+
+var sectionIndex = 0;
+
+rightArrow.addEventListener('click', function(){
+    sectionIndex = (sectionIndex < 3) ? sectionIndex +1 :3;
+    slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+});
+
+leftArrow.addEventListener('click', function(){
+    sectionIndex = (sectionIndex > 3) ? sectionIndex - 1 :0;
+    slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+});
+
+
 const hamburger= document.querySelector(".hamburger");
 const navItem= document.querySelector(".nav-menu");
 
